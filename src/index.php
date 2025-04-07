2c4840f209560b02f8201d2904dbaadb58a02ba8
Marco <marco.a.f.811@gmail.com>
2025-04-07 10:37:18 -0300

    feat: Adicionado funcionalidade de busca e categorias nas obras

--- src/index.php
+++ src/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
   <meta charset="UTF-8">
@@ -11,15 +11,44 @@
 
 
 <body>
-  <h1 class="titulo">Casco Cultural</h1>
-  <div class="grid-container">
-    <div class="img-container"><img class="img" src="assets/img/monalisa.png" alt="Monalisa"></div>
-    <div class="img-container"><img class="img" src="assets/img/monalisa.png" alt="Monalisa"></div>
-    <div class="img-container"><img class="img" src="assets/img/monalisa.png" alt="Monalisa"></div>
-    <div class="img-container"><img class="img" src="assets/img/monalisa.png" alt="Monalisa"></div>
-    <div class="img-container"><img class="img" src="assets/img/monalisa.png" alt="Monalisa"></div>
-    <div class="img-container"><img class="img" src="assets/img/monalisa.png" alt="Monalisa"></div>
-  </div>
+  <header>
+    <div class="search-container">
+      <input type="text" class="search-input" placeholder="Pesquisar obras...">
+      <button class="search-button">
+        <img src="assets/img/search.svg" alt="Buscar">
+      </button>
+    </div>
+  </header>
+
+  <main>
+    <h1 class="titulo">Casco Cultural</h1>
+    <div class="grid-container">
+      <div class="img-container">
+        <span class="categoria leonardo">Leonardo</span>
+        <img src="assets/img/monalisa.png" alt="Monalisa">
+      </div>
+      <div class="img-container">
+        <span class="categoria donatello">Donatello</span>
+        <img src="assets/img/monalisa.png" alt="Monalisa">
+      </div>
+      <div class="img-container">
+        <span class="categoria michelangelo">Michelangelo</span>
+        <img src="assets/img/monalisa.png" alt="Monalisa">
+      </div>
+      <div class="img-container">
+        <span class="categoria rafael">Rafael</span>
+        <img src="assets/img/monalisa.png" alt="Monalisa">
+      </div>
+      <div class="img-container">
+        <span class="categoria leonardo">Leonardo</span>
+        <img src="assets/img/monalisa.png" alt="Monalisa">
+      </div>
+      <div class="img-container">
+        <span class="categoria michelangelo">Michelangelo</span>
+        <img src="assets/img/monalisa.png" alt="Monalisa">
+      </div>
+    </div>
+  </main>
 </body>
 
 </html>
