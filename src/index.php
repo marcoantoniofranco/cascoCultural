68f5485c67dea667ecf41f4e28cf66d0143acb2e
Marco <marco.a.f.811@gmail.com>
2025-04-07 09:36:24 -0300

    feat: Estrutura básica de layout das imagens

--- src/index.php
+++ src/index.php
@@ -5,10 +5,21 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Casco Cultural</title>
+
+  <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
-<body>
 
+<body>
+  <h1 class="titulo">Casco Cultural</h1>
+  <div class="grid-container">
+    <div class="img-container"><img class="img" src="assets/img/monalisa.png" alt="Monalisa"></div>
+    <div class="img-container"><img class="img" src="assets/img/monalisa.png" alt="Monalisa"></div>
+    <div class="img-container"><img class="img" src="assets/img/monalisa.png" alt="Monalisa"></div>
+    <div class="img-container"><img class="img" src="assets/img/monalisa.png" alt="Monalisa"></div>
+    <div class="img-container"><img class="img" src="assets/img/monalisa.png" alt="Monalisa"></div>
+    <div class="img-container"><img class="img" src="assets/img/monalisa.png" alt="Monalisa"></div>
+  </div>
 </body>
 
 </html>
